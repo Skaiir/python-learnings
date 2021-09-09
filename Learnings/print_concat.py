4dbc7a0550ebca50d34ea3fdd42bc1e217c78bcb
Valentin Serra <serval.core@gmail.com>
2021-09-09 15:30:53 +0300

    First 30 minutes of Youtube Crash Course

---
+++ Learnings/print_concat.py
@@ -0,0 +1,7 @@
+person_name = "John"
+person_age = 27
+
+print(f"There was a man named {person_name}, ")
+print(f"he was {person_age} year old")
+print(f"He really liked the name {person_name}, ")
+print(f"but didn't like being {person_age}.")
